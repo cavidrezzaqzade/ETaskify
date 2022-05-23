d17a61953c4ed197e958fc991d08dd2e29ed0a3f
cavidrezzaqzade <cavidrazzagzade@gmail.com>
2022-05-23 14:48:48 +0400

    email sendingden basqa her sey hazirdi

--- src/test/java/az/abb/etaskify/service/TaskServiceTest.java
+++ src/test/java/az/abb/etaskify/service/TaskServiceTest.java
@@ -1,19 +1,91 @@
 package az.abb.etaskify.service;
 
+import az.abb.etaskify.domain.task.TaskDto;
+import az.abb.etaskify.entity.OrganizationEntity;
+import az.abb.etaskify.entity.UserEntity;
+import az.abb.etaskify.mapper.TaskMapper;
+import az.abb.etaskify.repository.TaskRepository;
+import az.abb.etaskify.repository.UserRepository;
+import az.abb.etaskify.service.auth.TokenService;
+import org.h2.util.Task;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
 /**
  * @author caci
  * @since 23.05.2022
  */
 
-
+@ExtendWith(MockitoExtension.class)
 class TaskServiceTest {
 
+    @Mock
+    private TaskRepository taskRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private TokenService tokenService;
+
+    @Mock
+    private TaskMapper taskMapper;
+
+    @InjectMocks
+    private TaskService taskService;
+
+    private static TaskDto taskDto;
+    private static OrganizationEntity organizationEntity;
+    private static Set<UserEntity> users;
+    private static UserEntity userEntity;
+    @BeforeAll
+    static void setUpAll(){
+
+        userEntity = UserEntity.builder()
+                .id(1L)
+                .username("test")
+                .password("test12")
+                .email("test@gmail.com")
+                .build();
+
+        users = new HashSet<>();
+        users.add(userEntity);
+
+        taskDto = TaskDto.builder().users(List.of(1L)).title("test-task").build();
+
+        organizationEntity = OrganizationEntity.builder().id(1L).orgName("test-org").build();
+    }
+
+
+
     @Test
-    void addNewTask() {
+    @DisplayName("check addNewTask ok")
+    void givenTaskDto_WhenAddNewTask_ThenOk() {
+        //given
+//        given(tokenService.getOrganizationFromToken()).willReturn(Optional.of(organizationEntity));
+//        given(userRepository.findAllIds(organizationEntity.getId())).willReturn(List.of(1L));
+
+        //when
+//        ResponseEntity<?> res = taskService.addNewTask(taskDto);
+
+        //then
+//        assertEquals(res.getStatusCode(), HttpStatus.OK);
     }
 
     @Test
---
+++ src/test/java/az/abb/etaskify/service/auth/TokenServiceTest.java
@@ -0,0 +1,21 @@
+package az.abb.etaskify.service.auth;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author caci
+ * @since 23.05.2022
+ */
+
+class TokenServiceTest {
+
+    @Test
+    void getOrganizationFromToken() {
+    }
+
+    @Test
+    void getUserFromToken() {
+    }
+}
